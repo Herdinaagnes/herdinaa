027d2a8a2d07ea09748ff770a8e588862501c2a5
Herdinaagnes <24051214174@mhs.unesa.ac.id>
2024-10-06 23:00:09 +0700

    Add files via upload

---
+++ Herdina Agnes P_24051214174.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2;
+    char operasi;
+
+    cout << "Masukkan angka pertama: ";
+    cin >> num1;
+    cout << "Masukkan operator (+, -, *, /): ";
+    cin >> operasi;
+    cout << "Masukkan angka kedua: ";
+    cin >> num2;
+
+    switch (operasi) {
+        case '+':
+            cout << "Hasil: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Hasil: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Hasil: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Hasil: " << num1 / num2 << endl;
+            else
+                cout << "Error: Pembagian dengan nol!" << endl;
+            break;
+        default:
+            cout << "Operator tidak valid!" << endl;
+            break;
+    }
+
+    return 0;
+}
